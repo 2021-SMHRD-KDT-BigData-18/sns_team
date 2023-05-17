f109ae58226bf5cfffc616494bdcb51d368f84a4
smhrd <smhrd@121.179.7.120>
2023-05-17 11:01:52 +0900

    Merge branch 'master' of https://github.com/2021-SMHRD-KDT-BigData-18/sns_team.git

---
+++ CoreProj/src/main/java/kr/smhrd/controller/FriendSelectCon.java
@@ -0,0 +1,20 @@
+package kr.smhrd.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FriendSelectCon implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+		
+		return null;
+	}
+
+}
--- CoreProj/src/main/java/kr/smhrd/dao/T_FRIENDDAO.java
+++ CoreProj/src/main/java/kr/smhrd/dao/T_FRIENDDAO.java
@@ -1,11 +1,22 @@
 package kr.smhrd.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import kr.smhrd.entity.T_FRIEND;
 
 public class T_FRIENDDAO {
 	
 	
 	private SqlSessionFactory factory = SqlSessionManager.getSqlSessionFactory();
 	
-	
+	public List<T_FRIEND> friendSelect(String U_ID) {
+
+		SqlSession session = factory.openSession(true);
+		List<T_FRIEND> list = session.selectList("friendSelect", U_ID);
+		session.close();
+		return list;
+	}
 }
